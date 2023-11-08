ec6330dcbd735ebbfc9993038624989532f32fcf
Agasthi98 <sankalana98@gmail.com>
2023-11-08 10:06:25 +0530

    reminer border color implemented

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -23,15 +23,32 @@ const Tasks = ({ onDelete }) => {
     },
   ]);
 
-  //delete task
+  //Delete task
   const deleteTask = (id) => {
-    console.log("delete", id);
+    setTasks(tasks.filter((task) => task.id !== id));
   };
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <>
-      {tasks.map((task) => (
-        <Task key={task.id} task={task} onDelete={deleteTask} />
-      ))}
+      {tasks.length > 0
+        ? tasks.map((task) => (
+            <Task
+              key={task.id}
+              task={task}
+              onDelete={deleteTask}
+              onToggle={toggleReminder}
+            />
+          ))
+        : "No Tasks To Show"}
     </>
   );
 };
